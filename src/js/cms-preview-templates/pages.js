86a4edda93fc11a18a5185fe1a96b3973b75c221
Ben Wilde <ben@well-made.uk>
2024-11-11 23:51:25 +0000

    Initial

---
+++ src/js/cms-preview-templates/pages.js
@@ -0,0 +1,20 @@
+import React from "react";
+import format from "date-fns/format";
+
+export default class PostPreview extends React.Component {
+  render() {
+    const {entry, widgetFor, getAsset} = this.props;
+
+    return <div className="mw6 center ph3 pv4">
+      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+      <div className="flex justify-between grey-3">
+        <p>{ format(entry.getIn(["data", "date"]), "iii, MMM d, yyyy") }</p>
+        <p>Read in x minutes</p>
+      </div>
+      <div className="cms mw6">
+        <p>{ entry.getIn(["data", "description"]) }</p>
+        { widgetFor("body") }
+      </div>
+    </div>;
+  }
+}
--- src/js/cms.js
+++ src/js/cms.js
@@ -2,20 +2,10 @@ import CMS from "decap-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 // eslint-disable-next-line import/no-unresolved
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.scss";
+import styles from "./cms-preview-templates/client-first-8950b2-1969c379705589633e6.webflow.css";
 
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
-import FooterPreview from "./cms-preview-templates/footer";
+import PagePreview from "./cms-preview-templates/pages";
 
 CMS.registerPreviewStyle(styles, {raw: true});
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
-CMS.registerPreviewTemplate("footer", FooterPreview);
+CMS.registerPreviewTemplate("pages", PagePreview);
 CMS.init();
