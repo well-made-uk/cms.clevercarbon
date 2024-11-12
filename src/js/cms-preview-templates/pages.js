fc0a5c1d488127ca6451fe3e1730fc1f3647b9bb
Ben Wilde <ben@well-made.uk>
2024-11-12 02:53:19 +0000

    Update hero section styling in pages.js
    Refactored the hero section to use flexbox for alignment and updated overall styles for better responsiveness and readability. Also adjusted the positions and dimensions of supplementary images to ensure precise layout alignment.

--- src/js/cms-preview-templates/pages.js
+++ src/js/cms-preview-templates/pages.js
@@ -9,22 +9,21 @@ export default class PostPreview extends React.Component {
 
     return (
       <div>
-        <div className="main-wrapper">
-          <div id="hero" className="section_hero">
-            <div className="flex gap-4rem vertical-align-center">
-              {headerLeft && <img src={headerLeft} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
-              <h1 style={{fontSize: "3.8rem", fontWeight: 700, lineHeight: 1, textAlign: "center", fontFamily: "Berlingske, sans-serif"}}>
-                {entry.getIn(["data", "title"])}
-              </h1>
-              {headerRight && <img src={headerRight} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
-            </div>
+        <div className="main-wrapper"
+          style={{position:"relative", maxWidth:"768px", fontSize:"19.2px", fontFamily: "\"Avenir Next\", sans-serif", lineHeight: "1.5"}}>
+          <div id="hero" className="section_hero" style={{display:"flex", gap:"4rem", alignItems:"center"}}>
+            {headerLeft && <img src={headerLeft} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
+            <h1 style={{fontSize: "3.8rem", fontWeight: 700, lineHeight: 1, textAlign: "center", fontFamily: "Berlingske, sans-serif"}}>
+              {entry.getIn(["data", "title"])}
+            </h1>
+            {headerRight && <img src={headerRight} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
           </div>
           <div id="content">
             {bitsAndPieces &&
                 <>
                   <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65f9e8ac9c5b161eb48cd056_Cloud-Left.svg"
                     loading="lazy" alt="" className="component_cloud right absolute-top-right"
-                    style={{willChange: "transform", transformStyle: "preserve-3d"}}/>
+                    style={{position: "absolute", top: "2%", left: "107.5%", width: "55vw", minWidth: "15rem", maxWidth: "40.5rem"}}/>
                   <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65fa0bdafea62ee7a5561015_Asset-55.svg"
                     loading="lazy" alt=""
                     style={{position: "absolute", top: "2rem", right: "112.5", width: "3.415rem",  maxWidth: "100%", display: "inline-block"}}/>
