31d16dc399d5ed60aec43eb3f72d30d1ccb1ebb3
Ben Wilde <ben@well-made.uk>
2024-11-12 00:22:01 +0000

    Refactor content rendering in templates
    Updated single.html to use .Params.content instead of .Content for better parameter handling. Enhanced pages.js to provide a more structured layout with new elements including header images and additional content sections.

--- src/js/cms-preview-templates/pages.js
+++ src/js/cms-preview-templates/pages.js
@@ -1,18 +1,50 @@
 import React from "react";
-//import format from "date-fns/format";
 
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor} = this.props;
+    const headerLeft = entry.getIn(["data", "header_left"]);
+    const headerRight = entry.getIn(["data", "header_right"]);
+    const bitsAndPieces = entry.getIn(["data", "bits_and_pieces"]);
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
-        <p>Read in x minutes</p>
+    return (
+      <div>
+        <div className="main-wrapper">
+          <div id="hero" className="section_hero">
+            <div className="padding-global-fixed">
+              <div className="container-small">
+                <div className="padding-section-small">
+                  <div className="flex gap-4rem vertical-align-center">
+                    {headerLeft && <img src={headerLeft} loading="lazy" alt="" className="max-width-5rem mobile-hide"/>}
+                    <h1 className="heading-style-h1 font-style-berlingske text-align-center">{entry.getIn(["data", "title"])}</h1>
+                    {headerRight && <img src={headerRight} loading="lazy" alt="" className="max-width-5rem mobile-hide"/>}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="content" className="section_content text-align-center">
+            <div className="padding-section-small">
+              <div className="padding-global">
+                <div className="container-small">
+                  <div className="padding-top padding-medium">
+                    {bitsAndPieces &&
+                        <>
+                          <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65f9e8ac9c5b161eb48cd056_Cloud-Left.svg"
+                            loading="lazy" alt="" className="component_cloud right absolute-top-right"
+                            style={{willChange: "transform", transformStyle: "preserve-3d"}}/>
+                          <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65fa0bdafea62ee7a5561015_Asset-55.svg"
+                            loading="lazy" alt="" className="component_stars absolute-top-left"/>
+                        </>
+                    }
+                    {widgetFor("content")}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="cms mw6">
-        { widgetFor("content") }
-      </div>
-    </div>;
+    );
   }
 }
