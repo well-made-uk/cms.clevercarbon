e63f9d31f05ae065f646fd40f6079eb7d14f5671
Ben Wilde <ben@well-made.uk>
2024-11-12 00:00:00 +0000

    Remove CSS injection in CMS preview
    Commented out the CSS import and registration for the CMS preview pane. This change might be temporary and is likely to address styling or performance issues in the CMS preview.

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,10 +2,10 @@ import CMS from "decap-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 // eslint-disable-next-line import/no-unresolved
-import styles from "./cms-preview-templates/client-first-8950b2-1969c379705589633e6.webflow.css";
+// import styles from "./cms-preview-templates/client-first-8950b2-1969c379705589633e6.webflow.css";
 
 import PagePreview from "./cms-preview-templates/pages";
 
-CMS.registerPreviewStyle(styles, {raw: true});
+// CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("pages", PagePreview);
 CMS.init();
