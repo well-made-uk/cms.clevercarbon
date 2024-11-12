7b540fbfc3511aa054ad79bb9e6cc29a123e78da
Ben Wilde <ben@well-made.uk>
2024-11-12 01:10:33 +0000

    Refactor layout structure and streamline styles
    Simplified the `single.html` and `pages.js` layout by removing redundant classes and containers. Enhanced maintainability by using inline styles and better structure in the HTML and JSX, ensuring streamlined and cleaner code without compromising the design.

--- src/js/cms-preview-templates/pages.js
+++ src/js/cms-preview-templates/pages.js
@@ -11,37 +11,26 @@ export default class PostPreview extends React.Component {
       <div>
         <div className="main-wrapper">
           <div id="hero" className="section_hero">
-            <div className="padding-global-fixed">
-              <div className="container-small">
-                <div className="padding-section-small">
-                  <div className="flex gap-4rem vertical-align-center">
-                    {headerLeft && <img src={headerLeft} loading="lazy" alt="" className="max-width-5rem mobile-hide"/>}
-                    <h1 className="heading-style-h1 font-style-berlingske text-align-center">{entry.getIn(["data", "title"])}</h1>
-                    {headerRight && <img src={headerRight} loading="lazy" alt="" className="max-width-5rem mobile-hide"/>}
-                  </div>
-                </div>
-              </div>
+            <div className="flex gap-4rem vertical-align-center">
+              {headerLeft && <img src={headerLeft} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
+              <h1 style={{fontSize: "3.8rem", fontWeight: 700, lineHeight: 1, textAlign: "center", fontFamily: "Berlingske, sans-serif"}}>
+                {entry.getIn(["data", "title"])}
+              </h1>
+              {headerRight && <img src={headerRight} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
             </div>
           </div>
-          <div id="content" className="section_content text-align-center">
-            <div className="padding-section-small">
-              <div className="padding-global">
-                <div className="container-small">
-                  <div className="padding-top padding-medium">
-                    {bitsAndPieces &&
-                        <>
-                          <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65f9e8ac9c5b161eb48cd056_Cloud-Left.svg"
-                            loading="lazy" alt="" className="component_cloud right absolute-top-right"
-                            style={{willChange: "transform", transformStyle: "preserve-3d"}}/>
-                          <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65fa0bdafea62ee7a5561015_Asset-55.svg"
-                            loading="lazy" alt="" className="component_stars absolute-top-left"/>
-                        </>
-                    }
-                    {widgetFor("body")}
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div id="content">
+            {bitsAndPieces &&
+                <>
+                  <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65f9e8ac9c5b161eb48cd056_Cloud-Left.svg"
+                    loading="lazy" alt="" className="component_cloud right absolute-top-right"
+                    style={{willChange: "transform", transformStyle: "preserve-3d"}}/>
+                  <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65fa0bdafea62ee7a5561015_Asset-55.svg"
+                    loading="lazy" alt=""
+                    style={{position: "absolute", top: "2rem", right: "112.5", width: "3.415rem",  maxWidth: "100%", display: "inline-block"}}/>
+                </>
+            }
+            {widgetFor("body")}
           </div>
         </div>
       </div>
