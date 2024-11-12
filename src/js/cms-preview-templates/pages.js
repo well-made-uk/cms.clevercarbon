09872d8cb29eb499576a86b5eeb730a7e1aac0d4
Ben Wilde <ben@well-made.uk>
2024-11-12 03:09:39 +0000

    Enhance image URL handling and adjust styles
    Introduced a base URL for image sources to ensure they are correctly loaded, particularly helpful when deployed under different paths. Also made several styling adjustments to center content and transform specific elements to improve the layout.

--- src/js/cms-preview-templates/pages.js
+++ src/js/cms-preview-templates/pages.js
@@ -8,23 +8,25 @@ export default class PostPreview extends React.Component {
     const headerRight = entry.getIn(["data", "header_right"]);
     const bitsAndPieces = entry.getIn(["data", "bits_and_pieces"]);
 
+    const baseUrl = window.CMS_PUBLIC_PATH || '';
+
     return (
       <div>
         <div className="main-wrapper"
-          style={{position:"relative", maxWidth:"768px", fontSize:"16px", fontFamily: "\"Avenir Next\", sans-serif", lineHeight: "1.5"}}>
-          <div id="hero" className="section_hero" style={{display:"flex", gap:"4rem", alignItems:"center"}}>
-            {headerLeft && <img src={headerLeft} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
+          style={{position:"relative", maxWidth:"768px", margin: "auto", fontSize:"16px", fontFamily: "\"Avenir Next\", sans-serif", lineHeight: "1.5"}}>
+          <div id="hero" className="section_hero" style={{display:"flex", gap:"4rem", alignItems:"center", justifyContent: "center"}}>
+            {headerLeft && <img src={`${baseUrl}${headerLeft}`} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
             <h1 style={{fontSize: "3.8rem", fontWeight: 700, lineHeight: 1, textAlign: "center", fontFamily: "Berlingske, sans-serif"}}>
               {entry.getIn(["data", "title"])}
             </h1>
-            {headerRight && <img src={headerRight} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
+            {headerRight && <img src={`${baseUrl}${headerRight}`} loading="lazy" alt="" style={{maxWidth:"5rem", width:"100%"}}/>}
           </div>
           <div id="content">
             {bitsAndPieces &&
                 <>
                   <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65f9e8ac9c5b161eb48cd056_Cloud-Left.svg"
                     loading="lazy" alt="" className="component_cloud right absolute-top-right"
-                    style={{position: "absolute", top: "2%", left: "107.5%", width: "55vw", minWidth: "15rem", maxWidth: "40.5rem"}}/>
+                    style={{transform: "translate3d(0px, -5.3232vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(180deg) rotateZ(0deg) skew(0deg, 0deg)", position: "absolute", top: "2%", left: "107.5%", width: "55vw", minWidth: "15rem", maxWidth: "40.5rem"}}/>
                   <img src="https://cdn.prod.website-files.com/65c771cc39ce7106362f82a4/65fa0bdafea62ee7a5561015_Asset-55.svg"
                     loading="lazy" alt=""
                     style={{position: "absolute", top: "2rem", right: "112.5", width: "3.415rem",  maxWidth: "100%", display: "inline-block"}}/>
